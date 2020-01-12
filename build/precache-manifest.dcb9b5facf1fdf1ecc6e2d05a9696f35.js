a8c69dd0b55e35eaefc9739af8065b596f33e6d5
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2020-01-12 01:03:40 +0100

    Build

---
+++ build/precache-manifest.dcb9b5facf1fdf1ecc6e2d05a9696f35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b98430ae9e9e46bb3d8493e68ee385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66a2f8938f048016e504",
+    "url": "/static/css/2.86b33428.chunk.css"
+  },
+  {
+    "revision": "d5b677da7a02008f760d",
+    "url": "/static/css/main.44a4ac7c.chunk.css"
+  },
+  {
+    "revision": "66a2f8938f048016e504",
+    "url": "/static/js/2.7f0e2b4c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.7f0e2b4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5b677da7a02008f760d",
+    "url": "/static/js/main.02ef612b.chunk.js"
+  },
+  {
+    "revision": "c60eed193642e24fdc3c",
+    "url": "/static/js/runtime-main.cf115a7a.js"
+  }
+]);
